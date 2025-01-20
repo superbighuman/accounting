1f8db6332230fdace7e8cf801dfd438a3b509f6d
d.kostukov <dimon20040611@gmail.com>
2025-01-20 12:15:02 +0300

    add CLI

--- src/main/java/ru/sfedu/accounting/Main.java
+++ src/main/java/ru/sfedu/accounting/Main.java
@@ -1,7 +1,47 @@
 package ru.sfedu.accounting;
-
+import java.util.Scanner;
 public class Main {
+    private static boolean running = true;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String version = "0.1";
+        String helloMessage = "Добро пожаловать в программу учета v %s,\n чтобы узнать команды, введите help".formatted(version);
+        System.out.println(helloMessage);
+        while(running){
+            pooling();
+        }
     }
+    private static void pooling(){
+        Scanner sc = new Scanner(System.in);
+        String query = sc.nextLine();
+        String[] params = query.split(" ");
+        if (params.length == 0)
+            return;
+        else if(params[0].equals("help")){
+            showHelp();
+            return;
+        }
+    }
+    private static void showHelp() {
+        String help = "Введите следующие команды, чтобы обратиться к соответствующему меню:";
+        String user = "user - добавление, изменение, удаление информации о пользователе";
+        String technic = "technic - добавление, изменение, удаление информации о технике";
+        String request = "request - добавление, изменение, удаление информации о запросах";
+        String repair = "repair - добавление, изменение, удаление информации о ремонтах";
+        String repairService = "repairService - добавление, изменение, удаление информации о сервисах ремонта";
+        String processor = "processor - добавление, изменение, удаление информации о процессорах";
+        String ram = "ram - добавление, изменение, удаление информации о модулях оперативной памяти";
+        String permanentMemory = "permanentMemory - добавление, изменение, удаление информации о накопителях";
+
+        System.out.println(help);
+        System.out.println(user);
+        System.out.println(technic);
+        System.out.println(request);
+        System.out.println(repair);
+        System.out.println(repairService);
+        System.out.println(processor);
+        System.out.println(ram);
+        System.out.println(permanentMemory);
+    }
+
+
 }
